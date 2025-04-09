235d7c052d4df40b8b119aecac41ba8af9f8697f
Akshaya Shaji <akshayashaji66@gmail.com>
2025-04-09 11:25:36 +0530

    note app comppleted with corrrection

--- NoteApp/scripts.js
+++ NoteApp/scripts.js
@@ -1,10 +1,18 @@
 const addBtn = document.querySelector(".add");
 
-addBtn.addEventListener('click',() => {
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if (notes) {
+    notes.forEach(note => {
+        addNewNote(note);
+    })
+}
+
+addBtn.addEventListener('click', () => {
     addNewNote();
 })
 
-function addNewNote() {
+function addNewNote(text = " ") {
     const note = document.createElement('div')
     note.classList.add('note')
 
@@ -14,7 +22,8 @@ function addNewNote() {
             <button class="edit"><i class="fas fa-edit"></i></button>
             <button class="delete"><i class="fas fa-trash-alt"></i></button>
         </div>
-        <div class="main hidden"></div>
+        <div class="main ${text ? "" : "hidden"}"></div>
+         <div class="main ${text ? "hidden" : " "}"></div>
         <textarea></textarea>
     </div>`
     const editBtn = note.querySelector('.edit')
@@ -23,21 +32,35 @@ function addNewNote() {
     const main = note.querySelector('.main')
     const textArea = note.querySelector('textarea')
 
+    textArea.value = text
+    main.innerHTML = marked(text)
+
     editBtn.addEventListener('click', () => {
         main.classList.toggle("hidden");
         textArea.classList.toggle("hidden");
     })
 
-    deleteBtn.addEventListener('click',()=>{
+    deleteBtn.addEventListener('click', () => {
         note.remove();
     })
     textArea.addEventListener("input", (e) => {
         const { value } = e.target;
         main.innerHTML = marked(value)
         // main.innerHTML=value  
+
     })
 
     document.body.appendChild(note)
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+    const notes = [];
+
+    notesText.forEach(note => {
+        notesText.push(note.value)
+    });
+    localStorage.setItem('notes', JSON.stringify(notes))
 }
 
 
